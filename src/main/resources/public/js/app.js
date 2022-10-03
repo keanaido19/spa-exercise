fb59dc1ca9a9efb71a8f779b55f05ac32a249077
keanaido021 <keanaido021@student.wethinkcode.co.za>
2022-10-03 02:09:26 +0200

    Passed AntonymTests

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -28,6 +28,18 @@ function lookupWord() {
   });
 }
 
+function removeDuplicates(dictionary, string) {
+  var returnList = [];
+  for (const [key, value] of Object.entries(dictionary)) {
+    for (const [key2, value2] of Object.entries(value)) {
+      if (key2 === string) {
+        returnList = returnList.concat(value2);
+      }
+    }
+  }
+  return [...new Set(returnList)];
+}
+
 function lookupSynonyms() {
   const form = document.getElementById("form");
   form.addEventListener("submit", (event) => {
@@ -47,9 +59,37 @@ function lookupSynonyms() {
         .then(data => {
           data = {
             word: data[0].word,
-            meanings: data[0].meanings
+            synonyms: removeDuplicates(data[0].meanings, 'synonyms')
           };
           const template = document.getElementById('synonyms-results-template').innerText;
+          const compiledFunction = Handlebars.compile(template);
+          document.getElementById('results').innerHTML = compiledFunction(data);
+        });
+  });
+}
+
+function lookupAntonyms() {
+  const form = document.getElementById("form");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const data = new FormData(event.target);
+    const word = data.get("word");
+
+    const options = {
+      method: 'GET',
+    };
+
+    document.getElementById('results').innerHTML = `<p>Searching for antonyms of <em>${word}</em>...</p>`;
+
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`, options)
+        .then(response => response.json())
+        .then(data => {
+          data = {
+            word: data[0].word,
+            antonyms: removeDuplicates(data[0].meanings, 'antonyms')
+          };
+          const template = document.getElementById('antonyms-results-template').innerText;
           const compiledFunction = Handlebars.compile(template);
           document.getElementById('results').innerHTML = compiledFunction(data);
         });
@@ -61,6 +101,7 @@ window.addEventListener('load', () => {
 
   const dictionaryTemplate = Handlebars.compile($('#dictionary-template').html());
   const synonymsTemplate = Handlebars.compile($('#synonyms-template').html());
+  const antonymsTemplate = Handlebars.compile($('#antonyms-template').html());
 
   const router = new Router({
     mode:'hash',
@@ -81,6 +122,12 @@ window.addEventListener('load', () => {
     html = synonymsTemplate();
     app.html(html);
     lookupSynonyms();
+  });
+
+  router.add('/antonyms', async () => {
+    html = antonymsTemplate();
+    app.html(html);
+    lookupAntonyms();
   });
 
   router.addUriListener();
